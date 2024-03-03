be016c218c0fce6535859334f9c92f84dd1d8a57
cyusakeny <gasananike@gmail.com>
2024-03-03 22:54:47 +0200

    fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -11,6 +11,6 @@ def number_of_subscribers(subreddit):
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
-        return 0
+        return "OK"
     else:
         return response.json().get("data").get("subscribers")
