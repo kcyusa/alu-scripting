6e8c87e6ce1841f7bc00366d54c6beb2fc476253
cyusakeny <gasananike@gmail.com>
2024-03-04 00:18:11 +0200

    fix

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -4,7 +4,7 @@ Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
-
+import sys
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
@@ -16,4 +16,4 @@ def number_of_subscribers(subreddit):
         subscribers = data['data']['subscribers']
         return subscribers
     else:
-        return "OK"
+        return 0
